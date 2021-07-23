88d80c632418235732d57571eb46851f0585396e
liang <xiaoleliang045@gmail.com>
2021-07-23 22:17:23 +0900

    basic Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,72 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Link,
   Redirect
 } from "react-router-dom";
+import styled from 'styled-components';
 
+// 100vh(view height) means always full fill screen
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
 
+const Main = styled.div`
+  border: 1px solid green;
+  flex-grow: 1; // this property can let element posses most space
+`
+
+const Nav = styled.div`
+  border: 1px solid blue;
+  >ul {
+    display: flex;
+    > li {
+      width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`
 function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/money">money</Link>
-          </li>
-          <li>
-            <Link to="/detail">detail</Link>
-          </li>
-          <li>
-            <Link to="/report">report</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        <Switch>
-          <Redirect exact from="/" to="/money"/>
-          <Route path="/money">
-            <Money />
-          </Route>
-          <Route path="/detail">
-            <Detail />
-          </Route>
-          <Route path="/report">
-            <Report />
-          </Route>
-          <Route path="*">
-            <NotFound/>
-          </Route>
-
-        </Switch>
-      </div>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/detail">
+              <Detail />
+            </Route>
+            <Route path="/report">
+              <Report />
+            </Route>
+            <Route path="*">
+              <NotFound/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/money">money</Link>
+            </li>
+            <li>
+              <Link to="/detail">detail</Link>
+            </li>
+            <li>
+              <Link to="/report">report</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
     </Router>
   );
 }
