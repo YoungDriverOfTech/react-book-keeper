728e336fe27311c2193fc416b9f0ac73b7e0ac39
liang <xiaoleliang045@gmail.com>
2021-07-24 12:07:15 +0900

    finish Nav Icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import {
     Link
 } from "react-router-dom";
+require('icons/money.svg');
+require('icons/detail.svg');
+require('icons/report.svg');
 
 const NavWrapper = styled.div`
 line-height: 24px;
@@ -12,7 +15,14 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > li {
     width: 33.3333%;
     text-align: center;
-    padding: 16px;
+    padding: 4px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .icon {
+      width: 24px;
+      height: 24px;
+    }
   }
 }
 `
@@ -22,12 +32,21 @@ const Nav = () => {
         <NavWrapper>
           <ul>
             <li>
+              <svg className="icon">
+                <use xlinkHref="#money"/>
+              </svg>
               <Link to="/money">money</Link>
             </li>
             <li>
+              <svg className="icon">
+                <use xlinkHref="#detail"/>
+              </svg>
               <Link to="/detail">detail</Link>
             </li>
             <li>
+              <svg className="icon">
+                <use xlinkHref="#report"/>
+              </svg>
               <Link to="/report">report</Link>
             </li>
           </ul>
